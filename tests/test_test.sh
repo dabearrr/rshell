d2a1fc23f9643c6129f1616ed3b48e986f93628b
Aakash Kadakia <aakash95@gmail.com>
2016-03-05 15:21:53 -0800

    test command script updated

--- tests/test_test.sh
+++ tests/test_test.sh
@@ -1,34 +1,125 @@
 #!/bin/sh
 #tests for the test command
 
+echo "Following test cases should result in: NOT FOUND"
+
 echo "Test #1"
 if test -e /test/file/path ; then
-	echo "Yes, found!"
+	   echo "Yes, found!"
 else
-	echo "Not found!"
+	   echo "Not found!"
 fi
 
 echo "Test #2"
 if [ -e /test/file/path ] ; then
-	echo "Yes, found!"
+	  echo "Yes, found!"
 else
-	echo "Not found!"
+	  echo "Not found!"
 fi
 
 echo "Test #3"
-test -f /test/file/path
+if test -f /test/file/path ; then
+	   echo "Yes, found!"
+else
+	   echo "Not found!"
+fi
 
 echo "Test #4"
-[ -f /test/file/path ]
+if [ -f /test/file/path ] ; then
+	  echo "Yes, found!"
+else
+	  echo "Not found!"
+fi
 
 echo "Test #5"
-test -d /test/file/path
+if test -d /test/file/path ; then
+	   echo "nope failed test!"
+else
+	   echo "right result!!!"
+fi
 
 echo "Test #6"
-[ -d /test/file/path ]
+if [ -d /test/file/path ] ; then
+	  echo "No not correct !"
+else
+	  echo "Right result!!"
+fi
 
 echo "Test #7"
-test /test/file/path
+if test -e /hello_world/path ; then
+	   echo "Yes, found!"
+else
+	   echo "Not found!"
+fi
 
 echo "Test #8"
-[ /test/file/path ]
+if [ -e /Hello_world/file/path ] ; then
+	  echo "Yes, found!"
+else
+	  echo "Not found!"
+fi
+
+echo "-------------------------------------------"
+
+echo "Following test cases should result in: YES, FOUND"
+
+echo "Test #9"
+if test -e ../tests/test_test.sh ; then
+	   echo "Yes, found!"
+else
+	   echo "Not found!"
+fi
+
+echo "Test #10"
+if [ -e ../tests/test_test.sh ] ; then
+	  echo "Yes, found!"
+else
+	  echo "Not found!"
+fi
+
+echo "Test #11"
+if test -f ../tests/test_test.sh ; then
+	   echo "Yes, found!"
+else
+	   echo "Not found!"
+fi
+
+echo "Test #12"
+if [ -f ../tests/test_test.sh ] ; then
+	  echo "Yes, found!"
+else
+	  echo "Not found!"
+fi
+
+echo "Test #13"
+if test -d ../tests/test_test.sh ; then
+	   echo "Not, Not correct!"
+else
+	   echo "Yes correct!"
+fi
+
+echo "Test #14"
+if [ -d ../tests/test_test.sh ] ; then
+	  echo "No, not correct!"
+else
+	  echo "Yes, correct!"
+fi
+
+echo "Test #15"
+if test ../tests/test_test.sh ; then
+	   echo "Yes, found!"
+else
+	   echo "Not found!"
+fi
+
+echo "Test #16"
+if [ ../tests/test_test.sh ] ; then
+	  echo "Yes, found!"
+else
+	  echo "Not found!"
+fi
+
+echo "-----------------------------------------------"
+
+
+
